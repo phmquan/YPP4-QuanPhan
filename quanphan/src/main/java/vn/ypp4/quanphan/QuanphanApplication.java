9b3fffaeaebcd8455533c9af2198707974ffc95b
YPP - Quan Phan <phanhoangminhquan240402@gmail.com>
2025-07-22 23:44:11 +0700

    init testcase sheet and project

---
+++ quanphan/src/main/java/vn/ypp4/quanphan/QuanphanApplication.java
@@ -0,0 +1,13 @@
+package vn.ypp4.quanphan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanphanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanphanApplication.class, args);
+	}
+
+}
---
+++ quanphan/src/test/java/vn/ypp4/quanphan/QuanphanApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ypp4.quanphan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanphanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
