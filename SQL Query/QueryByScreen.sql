a7fb1d3ddc75616245afd361a0d31fbb289776f5
YPP - Quan Phan <phanhoangminhquan240402@gmail.com>
2025-07-23 15:47:46 +0700

    SQL Query Trello By Screen

---
+++ SQL Query/QueryByScreen.sql
@@ -0,0 +1,38 @@
+﻿-- Query này lấy thông tin về các boards (bảng/dự án) mà user có ID = 1 có quyền truy cập thông qua các workspace họ đã tạo.
+SELECT b.Name, b.Description, b.CreatedAt, b.CreatedBy, b.AccessedAt, b.IsStar,b.BackgroundUrl,b.WorkspaceId,b.Status, u.id as UserId, u.Username as UserName
+FROM Users u
+Join Workspaces w ON w.CreatedBy=u.Id
+Join Boards b on b.WorkspaceId=w.Id
+WHERE
+    u.Id=1
+
+-- Query lấy ra Workspace mà user ID = 1 là member
+SELECT w.Name,w.Type,w.Description,w.CreatedAt,u.Id as UserId, u.Username as UserName,
+       m.PermissionId
+FROM Members m
+JOIN Workspaces w ON w.Id = m.OwnerId
+JOIN Users u ON u.Id = m.UserId
+WHERE m.UserId = 1 
+  AND m.OwnerType = 'WORKSPACE' 
+
+-- Query lấy ra Board mà user ID = 1 là member
+SELECT b.Name, b.Description, b.CreatedAt, b.CreatedBy, b.AccessedAt, b.IsStar,
+       b.BackgroundUrl, b.WorkspaceId, b.Status, 
+       u.Id as UserId, u.Username as UserName,
+       m.PermissionId
+FROM Members m
+JOIN Boards b ON b.Id = m.OwnerId
+JOIN Users u ON u.Id = m.UserId
+WHERE m.UserId = 1 
+  AND m.OwnerType = 'BOARD'
+
+-- Query lấy ra 10 template categories
+SELECT TOP 10 Name,IconUrl
+FROM TemplateCategories 
+
+-- Query lấy ra New and notable templates
+SELECT t.Title,t.BackgroundUrl,t.CreatedAt,t.CreatedBy,t.Copied,t.Viewed,t.Description
+From 
+    Templates t
+ORDER BY
+    CreatedAT desc, Viewed desc, Copied desc
