a092a777a8cf6dfa52c0247179e035309de5c7dc
Quan Phan <phanhoangminhquan240402@gmail.com>
2025-08-06 00:48:10 +0700

    Update Cache ListTemplate

---
+++ SQL_Query/MsList_Query/002_MsListCacheListTemplate.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE CachedListTemplateJson (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    ListTemplateId INT NOT NULL,
+    CachedJson NVARCHAR(MAX),
+    CachedAt DATETIME NOT NULL DEFAULT GETDATE()
+);
+Go
+CREATE OR ALTER PROCEDURE sp_CacheTemplateJson
+    @ListTemplateId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @json NVARCHAR(MAX);
+
+    SELECT 
+        trow.Id AS rowid,
+        (
+            SELECT 
+                tcol.Id AS colid,
+                tcol.ColumnName,
+                sdt.Icon,
+                tcell.CellValue
+            FROM TemplateColumn tcol
+            INNER JOIN SystemDataType sdt ON tcol.SystemDataTypeId = sdt.Id
+            INNER JOIN TemplateSampleCell tcell 
+                ON tcol.Id = tcell.TemplateColumnId 
+                AND tcell.TemplateSampleRowId = trow.Id
+            WHERE tcol.ListTemplateId = @ListTemplateId
+            FOR JSON PATH
+        ) AS cells
+    FROM TemplateSampleRow trow
+    WHERE trow.ListTemplateId = @ListTemplateId
+    FOR JSON PATH, ROOT('rows');
+
+    -- Xoá cache cũ nếu có
+    DELETE FROM CachedListTemplateJson WHERE ListTemplateId = @ListTemplateId;
+
+    -- Lưu cache mới
+    INSERT INTO CachedListTemplateJson (ListTemplateId, CachedJson)
+    VALUES (@ListTemplateId, @json);
+END;
+Go
+EXEC sp_CacheTemplateJson @ListTemplateId = 2
